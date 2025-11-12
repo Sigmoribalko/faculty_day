08ed7c7b1ae612310e47808a5f225c521b4ae7bc
antoninanikulina <mkemke31269@gmail.com>
2025-11-12 23:57:27 +0300

    Реализован калькулятор

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -3,8 +3,25 @@ package ru.tbank.education.school.lesson1
 /**
  * Метод для вычисления простых арифметических операций.
  */
-fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+fun calculate(a: Double, b: Double, operation: OperationType = OperationType.ADD): Double? {
+    val result = when (operation) {
+        OperationType.ADD -> a + b
+        OperationType.SUBTRACT -> a - b
+        OperationType.MULTIPLY -> a * b
+        OperationType.DIVIDE -> {
+            if (b == 0.0) {
+                null
+            } else {
+                a / b
+            }
+        }
+    }
+    if (result != null) {
+    println(result)
+    } else {
+        println("Ошибка: деление на ноль")
+    }
+    return result
 }
 
 /**
@@ -14,5 +31,28 @@ fun calculate(a: Double, b: Double, operation: OperationType): Double? {
  */
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    val sumbls = this.split(" ")
+    
+    if (sumbls.size != 3) return null
+    
+    val aTemp = sumbls[0].toDoubleOrNull()
+    if (aTemp == null) {
+        return null
+    }
+    val a = aTemp
+    
+    val bTemp = sumbls[2].toDoubleOrNull()
+    if (bTemp == null) {
+        return null
+    }
+    val b = bTemp
+    
+    val operation = when (sumbls[1]) {
+        "+" -> OperationType.ADD
+        "-" -> OperationType.SUBTRACT
+        "*" -> OperationType.MULTIPLY
+        "/" -> OperationType.DIVIDE
+        else -> return null
+    }
+    return calculate(a, b, operation)
 }
